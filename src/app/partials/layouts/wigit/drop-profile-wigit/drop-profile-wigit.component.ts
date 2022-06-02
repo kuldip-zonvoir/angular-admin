1f5ff92cf4c9363dcbc122e68ceb195a7d07dc87
Kuldip zonvoir <kuldip@zonvoir.com>
2022-06-02 17:43:41 +0530

    all testing done @kuldip-zonvoir

--- src/app/partials/layouts/wigit/drop-profile-wigit/drop-profile-wigit.component.ts
+++ src/app/partials/layouts/wigit/drop-profile-wigit/drop-profile-wigit.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-drop-profile-wigit',
-  templateUrl: './drop-profile-wigit.component.html',
-  styleUrls: ['./drop-profile-wigit.component.scss']
+  selector: "app-drop-profile-wigit",
+  templateUrl: "./drop-profile-wigit.component.html",
+  styleUrls: ["./drop-profile-wigit.component.scss"],
 })
 export class DropProfileWigitComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  goToProfile() {
+    this.router.navigate(["account/profile-information"]);
   }
 
+  goToMail() {
+    this.router.navigate(["email/inbox"]);
+  }
+  goToPlans() {
+    this.router.navigate(["special-pages/price-plans"]);
+  }
+  goToTasks() {
+    this.router.navigate(["application/task-manager"]);
+  }
 }
--- src/app/partials/layouts/wigit/email-wigit/email-wigit.component.ts
+++ src/app/partials/layouts/wigit/email-wigit/email-wigit.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-email-wigit',
-  templateUrl: './email-wigit.component.html',
-  styleUrls: ['./email-wigit.component.scss']
+  selector: "app-email-wigit",
+  templateUrl: "./email-wigit.component.html",
+  styleUrls: ["./email-wigit.component.scss"],
 })
 export class EmailWigitComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  readEmailMessage() {
+    this.router.navigate(["email/read"]);
   }
-
 }
